ea6f8366d6c62cbe11d894ed22f86a61d2f2ccca
Christian Raoulis <chris@raoulis.de>
2024-08-27 20:03:55 +0200

    storage - fix register events don't trigger

--- storage-common/src/main/java/com/github/manasmods/manascore/storage/ManasCoreStorage.java
+++ storage-common/src/main/java/com/github/manasmods/manascore/storage/ManasCoreStorage.java
@@ -1,6 +1,8 @@
 package com.github.manasmods.manascore.storage;
 
+import com.github.manasmods.manascore.storage.impl.StorageManager;
 import com.github.manasmods.manascore.storage.impl.network.ManasStorageNetwork;
+import dev.architectury.event.events.common.LifecycleEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,5 +11,6 @@ public final class ManasCoreStorage {
 
     public static void init() {
         ManasStorageNetwork.init();
+        LifecycleEvent.SETUP.register(StorageManager::init);
     }
 }
