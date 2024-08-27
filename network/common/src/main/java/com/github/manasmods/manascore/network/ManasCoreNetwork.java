4a507eda43301a46a66ed808f25a97d779ae7612
Christian Raoulis <chris@raoulis.de>
2024-08-27 10:47:15 +0200

    remove network module

--- network/common/src/main/java/com/github/manasmods/manascore/network/ManasCoreNetwork.java
+++
@@ -1,7 +0,0 @@
-package com.github.manasmods.manascore.network;
-
-public class ManasCoreNetwork {
-    public static void init() {
-        // Write common init code here.
-    }
-}
--- network/fabric/src/main/java/com/github/manasmods/manascore/network/fabric/client/ManasCoreNetworkFabricClient.java
+++
@@ -1,9 +0,0 @@
-package com.github.manasmods.manascore.network.fabric.client;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class ManasCoreNetworkFabricClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-    }
-}
