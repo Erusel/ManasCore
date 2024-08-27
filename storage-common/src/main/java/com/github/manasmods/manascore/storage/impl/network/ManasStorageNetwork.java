68ee9dd520ab6a054e265a337e29b9955d9377d7
Christian Raoulis <chris@raoulis.de>
2024-08-27 19:26:31 +0200

    storage - use network utils for payload registration

--- storage-common/src/main/java/com/github/manasmods/manascore/storage/impl/network/ManasStorageNetwork.java
+++ storage-common/src/main/java/com/github/manasmods/manascore/storage/impl/network/ManasStorageNetwork.java
@@ -1,19 +1,14 @@
 package com.github.manasmods.manascore.storage.impl.network;
 
+import com.github.manasmods.manascore.network.api.util.NetworkUtils;
 import com.github.manasmods.manascore.storage.impl.network.s2c.SyncChunkStoragePayload;
 import com.github.manasmods.manascore.storage.impl.network.s2c.SyncEntityStoragePayload;
 import com.github.manasmods.manascore.storage.impl.network.s2c.SyncWorldStoragePayload;
-import dev.architectury.networking.NetworkManager;
 
 public class ManasStorageNetwork {
     public static void init() {
-        NetworkManager.registerS2CPayloadType(SyncChunkStoragePayload.TYPE, SyncChunkStoragePayload.STREAM_CODEC);
-        NetworkManager.registerReceiver(NetworkManager.Side.S2C, SyncChunkStoragePayload.TYPE, SyncChunkStoragePayload.STREAM_CODEC, SyncChunkStoragePayload::handle);
-
-        NetworkManager.registerS2CPayloadType(SyncEntityStoragePayload.TYPE, SyncEntityStoragePayload.STREAM_CODEC);
-        NetworkManager.registerReceiver(NetworkManager.Side.S2C, SyncEntityStoragePayload.TYPE, SyncEntityStoragePayload.STREAM_CODEC, SyncEntityStoragePayload::handle);
-
-        NetworkManager.registerS2CPayloadType(SyncWorldStoragePayload.TYPE, SyncWorldStoragePayload.STREAM_CODEC);
-        NetworkManager.registerReceiver(NetworkManager.Side.S2C, SyncWorldStoragePayload.TYPE, SyncWorldStoragePayload.STREAM_CODEC, SyncWorldStoragePayload::handle);
+        NetworkUtils.registerS2CPayload(SyncChunkStoragePayload.TYPE, SyncChunkStoragePayload.STREAM_CODEC, SyncChunkStoragePayload::handle);
+        NetworkUtils.registerS2CPayload(SyncEntityStoragePayload.TYPE, SyncEntityStoragePayload.STREAM_CODEC, SyncEntityStoragePayload::handle);
+        NetworkUtils.registerS2CPayload(SyncWorldStoragePayload.TYPE, SyncWorldStoragePayload.STREAM_CODEC, SyncWorldStoragePayload::handle);
     }
 }
