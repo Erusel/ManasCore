4d41b3b606d6704d9a4b468e89e8f418019cb1b3
Christian Raoulis <chris@raoulis.de>
2024-08-28 18:32:19 +0200

    storage - fix wrong package naming in fabric project

--- storage-fabric/src/main/java/com/github/manasmods/manascore/storage/fabric/ManasCoreStorageFabric.java
+++ storage-fabric/src/main/java/com/github/manasmods/manascore/storage/fabric/ManasCoreStorageFabric.java
@@ -1,9 +1,9 @@
-package com.github.manasmods.manascore.network.fabric;
+package com.github.manasmods.manascore.storage.fabric;
 
 import com.github.manasmods.manascore.storage.ManasCoreStorage;
 import net.fabricmc.api.ModInitializer;
 
-public class ManasCoreNetworkFabric implements ModInitializer {
+public class ManasCoreStorageFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         ManasCoreStorage.init();
