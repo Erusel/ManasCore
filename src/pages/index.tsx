4a7e89dabdd2744ef232c3921d18d497305f8e26
Christian Raoulis <chris@raoulis.de>
2024-08-29 15:27:41 +0200

    add empty paged for storage and network module

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/getting-started/installation">
+            to="/docs/storage/getting-started/Installation">
             Getting Started
           </Link>
         </div>
