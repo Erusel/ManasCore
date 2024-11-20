5f76f654d4f72dd17c4196890d000ed87a417142
Erusel <nassimeboukari73@gmail.com>
2024-11-20 15:39:17 +0100

    Fixed dumb fail mistake

--- inventory-fabric/src/main/java/io/github/manasmods/manascore/inventory/fabric/ManasCoreInventoryFabric.java
+++ inventory-fabric/src/main/java/io/github/manasmods/manascore/inventory/fabric/ManasCoreInventoryFabric.java
@@ -1,4 +1,9 @@
-package io.github.manasmods.manascore.config.fabric;
+/*
+ * Copyright (c) 2024. ManasMods
+ * GNU General Public License 3
+ */
+
+package io.github.manasmods.manascore.inventory.fabric;
 
 import io.github.manasmods.manascore.inventory.ManasCoreInventory;
 import net.fabricmc.api.ModInitializer;
