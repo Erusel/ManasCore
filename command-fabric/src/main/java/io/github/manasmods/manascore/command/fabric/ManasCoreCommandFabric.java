4fb5830beaf52d8cbf42650f5ce5028b25c28541
Chris <chris@raoulis.de>
2024-12-01 19:01:08 +0100

    add FabricPermissionsApiIntegration for luckperms

--- command-fabric/src/main/java/io/github/manasmods/manascore/command/fabric/ManasCoreCommandFabric.java
+++ command-fabric/src/main/java/io/github/manasmods/manascore/command/fabric/ManasCoreCommandFabric.java
@@ -7,7 +7,9 @@ package io.github.manasmods.manascore.command.fabric;
 
 import io.github.manasmods.manascore.command.ManasCoreCommand;
 import io.github.manasmods.manascore.command.api.Permission;
+import io.github.manasmods.manascore.command.fabric.integrations.FabricPermissionsApiIntegration;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.commands.CommandSourceStack;
 
 import java.util.function.BiFunction;
@@ -21,6 +23,11 @@ public class ManasCoreCommandFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        if (FabricLoader.getInstance().isModLoaded("fabric-permissions-api-v0")) {
+            hasPermission = FabricPermissionsApiIntegration.PERMISSION_CHECK;
+        }
+
+
         ManasCoreCommand.init();
     }
 }
